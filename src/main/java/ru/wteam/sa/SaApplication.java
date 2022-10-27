1f2e72a0c6bbc900e824ea6e9898744f40758e5f
Madzaki <veburg@mail.ru>
2022-10-28 01:07:57 +0300

    SA-57 add start project

---
+++ src/main/java/ru/wteam/sa/SaApplication.java
@@ -0,0 +1,13 @@
+package ru.wteam.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/wteam/sa/SaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wteam.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
